90a08b4991c9276c405aa1dffdd3c9e1281db003
Eduardo Mendez <62312874+inntekdeveloperti5@users.noreply.github.com>
2025-05-27 12:57:40 -0600

    Feat: Total chekout

--- src/components/CartCheckout.jsx
+++ src/components/CartCheckout.jsx
@@ -1,20 +1,43 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import PlantCheckout from "./PlantCheckou";
+import PlantCheckout from "./PlantCheckout";
 
 function CartCheckout() {
 
-    const plants = useSelector((state) => state.shopingCart.plants.flatMap(e => e.plants));
-    const basket = useSelector((state) => state.shopingCart.basket.map(item => item.id));
+    const plantsSource = useSelector((state) => state.shopingCart.plants);
+    const plants = plantsSource.flatMap(e => e.plants);
+    
+    const basket = useSelector((state) => state.shopingCart.basket);
+    const basketIds = basket.map(item => item.id);
     const [list, setList] = useState([]);
 
+    const [total, setTotal] = useState(0);
+
     useEffect(() => {
-        const current = plants.filter((plant)=> basket.find(item => item.id === plant.id));
+        
+        const current = plants.filter((plant)=> basketIds.includes(plant.id));
         setList(current);
+
+        if (current) {
+
+            let sum = 0;
+            
+            for (let item of current){
+                const basketCount = basket.find(b => b.id === item.id)?.count || 0;
+                sum += item.cost * basketCount;
+            }
+            
+            setTotal(sum);
+        }
+        else{
+            setTotal(0);
+        }
+
     }, [basket]);
 
     return (
         <dialog id="cartCheckoutDialog" className="bg-white rounded shadow-lg w-100 m-auto">
+            <h2 className="text-center text-2xl pt-4">Total: ${total.toLocaleString()}</h2>
             <form className='p-4' method="dialog">
                 <ul className="mb-4">
                     {list.map((plant) => <PlantCheckout key={plant.id} plant={plant} />)}
--- src/components/PlantCheckout.jsx
+++ src/components/PlantCheckout.jsx
@@ -6,7 +6,7 @@ function PlantCheckout(prop) {
 
     const plant = prop.plant;
     const [total, setTotal] = useState(plant.cost);
-    
+
     const dispatch = useDispatch();
     const onBtnRemoveClick = (id) => dispatch(removeFromCart(id));
 
@@ -14,6 +14,10 @@ function PlantCheckout(prop) {
     const basket = useSelector((state) => state.shopingCart.basket);
     const basketplant = basket.find(item => item.id === plant.id);
     const basketCount = basketplant?.count || 1;
+
+    useEffect(() => {
+        setTotal(plant.cost * basketCount);
+    }, [basket]);
 
     const btnAddItem = () => {
         if (basketCount >= 9) return;
@@ -25,6 +29,10 @@ function PlantCheckout(prop) {
         dispatch(removeItem(plant.id));
     }
 
+    const handleChange = (event) => {
+        console.log(event.target.value);
+    };
+
     return (
         <li className="flex flex-row border-b border-slate-200 gap-4 py-2 m-0">
             <figure className="flex-init size-25 rounded overflow-hidden">
@@ -35,11 +43,11 @@ function PlantCheckout(prop) {
                 <div className="flex flex-row rounded border border-slate-100">
                     <button type="button" className="bg-slate-100 rounded hover:bg-slate-200 cursor-pointer py-2 px-4"
                         onClick={btnRemoveItem}>-</button>
-                    <input type="number" min='0' max='9' value={basketCount} className="w-full text-center" required />
+                    <input type="number" min='0' max='9' value={basketCount} onChange={handleChange} className="w-full text-center" required />
                     <button type="button" className="bg-slate-100 rounded cursor-pointer hover:bg-slate-200 py-2 px-4"
                         onClick={btnAddItem}>+</button>
                 </div>
-                <p><bold className="font-semibold">Total:</bold> $ {total}</p>
+                <p><strong className="font-semibold">Total:</strong> $ {total}</p>
                 <button type="button" className="bg-red-600 rounded hover:bg-red-700 shadow px-4 py-2 text-white hover:bg-red- cursor-pointer"
                     onClick={() => onBtnRemoveClick(plant.id)}>
                     Delete
