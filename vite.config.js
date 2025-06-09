e99569b3015983f6d211ba7731b74a7757368212
inntekdeveloperti5 <desarrollo.ti5@grupofransun.com.mx>
2025-06-09 16:37:45 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/"
+  base: "/",
+  build:{
+    rollupOptions:{
+      input:{
+        index: './index.html',
+        shop: './shop.html'
+      }
+    }
+  }
 })
